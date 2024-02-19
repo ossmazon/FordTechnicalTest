a1cfe3287a365f29fe3b841926a5d59a96a558e1
Osvin Mazon Guzman <70389790+ossmazon@users.noreply.github.com>
2024-02-19 15:57:10 -0600

    Update Main.java
    handle first 5 scenarios

--- FordTest/src/Main.java
+++ FordTest/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void covert(int a){
+
+        switch (a){
+            case(1):
+                System.out.println("I");
+                break;
+            case(2):
+                System.out.println("II");
+                break;
+            case(3):
+                System.out.println("III");
+                break;
+            case(4):
+                System.out.println("IV");
+                break;
+            case(5):
+                System.out.println("V");
+                break;
+        }
     }
+
+    public static void main(String[] args) {
+
+        covert(5);
+
+    }
+
 }
