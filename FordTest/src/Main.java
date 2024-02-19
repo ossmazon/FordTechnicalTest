f1b4ef5aa656cd22acf69968d30b9799f5045901
Osvin Mazon Guzman <70389790+ossmazon@users.noreply.github.com>
2024-02-19 17:24:13 -0600

    fixed solution
    Handle all scenarios  from 0 to 3000

--- FordTest/src/Main.java
+++ FordTest/src/Main.java
@@ -1,30 +1,69 @@
 public class Main {
 
-    public static void covert(int a){
 
-        switch (a){
-            case(1):
-                System.out.println("I");
-                break;
-            case(2):
-                System.out.println("II");
-                break;
-            case(3):
-                System.out.println("III");
-                break;
-            case(4):
-                System.out.println("IV");
-                break;
-            case(5):
-                System.out.println("V");
-                break;
+    public static void covert(int a){
+        if(a>=1 & a <=3000) {
+            String result = "";
+            while (a >= 1000) {
+                result += "M";
+                a -= 1000;
+            }
+            while (a >= 900) {
+                result += "CM";
+                a -= 900;
+            }
+            while (a >= 500) {
+                result += "D";
+                a -= 500;
+            }
+            while (a >= 400) {
+                result += "CD";
+                a -= 400;
+            }
+            while (a >= 100) {
+                result += "C";
+                a -= 100;
+            }
+            while (a >= 90) {
+                result += "XC";
+                a -= 90;
+            }
+            while (a >= 50) {
+                result += "L";
+                a -= 50;
+            }
+            while (a >= 40) {
+                result += "XL";
+                a -= 40;
+            }
+            while (a >= 10) {
+                result += "X";
+                a -= 10;
+            }
+            while (a >= 9) {
+                result += "IX";
+                a -= 9;
+            }
+            while (a >= 5) {
+                result += "V";
+                a -= 5;
+            }
+            while (a >= 4) {
+                result += "IV";
+                a -= 4;
+            }
+            while (a >= 1) {
+                result += "I";
+                a -= 1;
+            }
+            System.out.println(result);
+        }else{
+            System.out.println("Number out of length");
         }
     }
 
     public static void main(String[] args) {
-
-        covert(5);
-
+        covert(852);
     }
 
 }
